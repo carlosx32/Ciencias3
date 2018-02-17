9a23c62edc585d1bbaf567ba03822a07f29408da
carlos <carlos0camacho@gmail.com>
2018-02-17 16:28:33 -0500

    Hospital

---
+++ clase_1/Estructuras.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+class Cola:
+    """ Representa una cola con operaciones de encolar, desencolar y
+    verificar si está vacía. """
+
+    def __init__(self):
+        """ Crea una cola vacía. """
+        # La cola vacía se representa con una lista vacía
+        self.items=[]
+
+    def encolar(self, x):
+        """ Agrega el elemento x a la cola. """
+        # Encolar es agregar al final de la cola.
+        self.items.append(x)
+
+    def desencolar(self):
+        """ Devuelve el elemento inicial y lo elimina de la cola.
+            Si la cola está vacía levanta una excepción. """
+        try:
+            return self.items.pop(0)
+        except IndexError:
+            raise ValueError("La cola está vacía")
+    def es_vacia(self):
+        """ Devuelve True si la lista está vacía, False si no. """
+        return self.items == []
+    def ver_cola(self):
+        try:
+            return self.items[0]
+        except IndexError:
+            raise ValueError("La cola está vacía")
+
+class Pila:
+    """ Representa una pila con operaciones de apilar, desapilar y
+        verificar si está vacía. """
+
+    def __init__(self):
+        """ Crea una pila vacía. """
+        # La pila vacía se representa con una lista vacía
+        self.items=[]
+
+    def apilar(self, x):
+        """ Agrega el elemento x a la pila. """
+        # Apilar es agregar al final de la lista.
+        self.items.append(x)
+
+    def desapilar(self):
+        """ Devuelve el elemento tope y lo elimina de la pila.
+            Si la pila está vacía levanta una excepción. """
+        try:
+            return self.items.pop()
+        except IndexError:
+            raise ValueError("La pila está vacía")
+
+    def es_vacia(self):
+        """ Devuelve True si la lista está vacía, False si no. """
+        return self.items == []
+    def ver_pila(self):
+        try:
+            return self.items[len(self.items)-1]
+        except IndexError:
+            raise ValueError("La lista esta vacia")
--- clase_1/Hospital.py
+++ clase_1/Hospital.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+from Estructuras import Cola
+from Estructuras import Pila
+
 class Persona(object):
     """docstring for Personas."""
     def __init__(self, documento,nombre,especialidad):
-        self.documento = documento
+        self.documento = str(documento)
         self.nombre = nombre
         self.especialidad = especialidad
     def getNombre(self):
@@ -10,29 +14,123 @@ class Persona(object):
         return self.documento
     def getEspecialidad(self):
         return self.especialidad
-class ColaHop(object):
-    """Cola segun la especialidad"""
-    def __init__(self,especialidad):
-        self.especialidad=especialidad
-        self.pacientes=[]
-    def nuevoPaciente(self, x):
-        self.pacientes.append(x);
-    def desencolarCliente(self):
-        try:
-            return self.pacientes.pop(0)
-        except IndexError:
-            raise ValueError("No hay clientes")
-    def es_vacia(self):
-        return self.items == []
+    def getPersona(self):
+        return "Doc: " + self.documento + " nombre: "+self.nombre +" Especialidad: "+self.especialidad
+class Gestion(object):
+    def __init__(self):
+        self.pacientes=Cola()
+        self.pacientesAtendidos=Pila()
+        self.historialPacientes=Pila()
+        self.cardiologia= Cola()
+        self.neumologia= Cola()
+        self.fisioterapia=Cola()
+        self.otras=Cola()
+    def espera(self,paciente):
+        self.pacientes.encolar(paciente)
+        self.historialPacientes.apilar(paciente)
+        self.asignarEspecialidades()
+    def asignarEspecialidades(self):
+        while not self.pacientes.es_vacia():
+            if(self.pacientes.ver_cola().getEspecialidad()=='cardiologia'):
+                self.cardiologia.encolar(self.pacientes.desencolar())
+            elif(self.pacientes.ver_cola().getEspecialidad()=='neumologia'):
+                self.neumologia.encolar(self.pacientes.desencolar())
+            elif(self.pacientes.ver_cola().getEspecialidad()=='fisioterapia'):
+                self.fisioterapia.encolar(self.pacientes.desencolar())
+            else:
+                self.otras.encolar(self.pacientes.desencolar())
+    def atender(self, nEspecialidad):
+        if(nEspecialidad==1):
+            if not self.cardiologia.es_vacia():
+                self.pacientesAtendidos.apilar(self.cardiologia.ver_cola())
+                return self.cardiologia.desencolar()
+        elif(nEspecialidad==2):
+            if not self.neumologia.es_vacia():
+                self.pacientesAtendidos.apilar(self.neumologia.ver_cola())
+                return self.neumologia.desencolar()
+        elif(nEspecialidad==3):
+            if not  self.neumologia.es_vacia():
+                self.pacientesAtendidos.apilar(self.fisioterapia.ver_cola())
+                return self.fisioterapia.desencolar()
+        else:
+            if not self.otras.es_vacia():
+                self.pacientesAtendidos.apilar(self.otras.ver_cola())
+                return self.otras.desencolar()
+        return ""
+    def getHistorial(self):
+        return self.historialPacientes
+    def getAtendidos(self):
+        return self.pacientesAtendidos
+    def getCardiologia(self):
+        return self.cardiologia
+    def getNeumologia(self):
+        return self.neumologia
+    def getFisioterapia(self):
+        return self.fisioterapia
+    def getOtras(self):
+        return self.otras
 
-ListPacientes=[]
-ListPacientes.append(Persona(1,'persona1','esp1'))
-ListPacientes.append(Persona(2,'persona2','esp2'))
-ListPacientes.append(Persona(3,'persona3','esp1'))
+#menu
+hospital=Gestion();
+##datos prueba
+hospital.espera(Persona(1,'persona1','cardiologia'))
+hospital.espera(Persona(2,'persona2','fisioterapia'))
+hospital.espera(Persona(3,'persona3','cardiologia'))
+hospital.espera(Persona(4,'persona4','medicina general'))
+hospital.espera(Persona(5,'persona5','neumologia'))
+hospital.espera(Persona(6,'persona6','medicina general'))
 
-esp={'cardiologia':ListPacientes[0] ,'cardiologia':ListPacientes[0] }
+a=True
+auxCola=Cola()
 
+while a:
+    print("Elija una opcion")
+    print("1 nuevo paciente")
+    print("2 atender")
+    print("3 pacientes en espera")
+    print("4 pacientes atendidos")
+    print("5 historial")
+    print("6 salir")
+    opcion=input("Selecciones una opcion:")
+    if(opcion==1):
+        hospital.espera(Persona(raw_input("Ingrese el documento: "),raw_input("Ingrese el nombre: "), raw_input("Ingrese la especialidad: ")))
+    elif(opcion==2):
+        print("         atender pacientes:       ")
+        opcAtender=input("      ingrese 1 para para cardiologia, 2 para neumologia 3 para fisioterapia, 4 para otras : ")
+        auxPersona=hospital.atender(opcAtender)
+        if not auxPersona=="":
+            print("     Datos del paciente   "+ auxPersona.getPersona())
+        else:
+            print("     No hay pacientes")
+    elif(opcion==3):
+        print("         pacientes en espera:       ")
+        print("     pacientes cardiologia")
+        auxCola=hospital.getCardiologia()
+        while not auxCola.es_vacia:
+            print(auxCola.desencolar().getPersona())
+        print("     pacientes neumologia")
+        auxCola=hospital.getNeumologia()
+        while not auxCola.es_vacia:
+            print(auxCola.desencolar().getPersona())
+        print("     pacientes fisioterapia")
+        auxCola=hospital.getFisioterapia()
+        while not auxCola.es_vacia:
+            print(auxCola.desencolar().getPersona())
+        print("     pacientes otras")
+        auxCola=hospital.getOtras()
+        while not auxCola.es_vacia:
+            print(auxCola.desencolar().getPersona())
+        print("fin")
+    elif opcion==4:
+        print("         Pacientes atendidos:       ")
+        auxCola=hospital.getAtendidos()
+        while not auxCola.es_vacia():
+            print("             "+auxCola.desapilar().getPersona())
 
-for x in ListPacientes:
-    print(x.getNombre(), x.getEspecialidad())
-"""Asignamos las personas a las colas"""
+    elif(opcion==5):
+        auxCola=hospital.getHistorial()
+        print("         Historial:       ")
+        while not auxCola.es_vacia():
+            print("             "+auxCola.desapilar().getPersona())
+    else:
+        a=False
